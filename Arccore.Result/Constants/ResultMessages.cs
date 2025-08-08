5c5afaa94f4de204f3a530d518e98f19718a7e2c
alaaradman <151939811+alaaradman@users.noreply.github.com>
2025-08-08 03:10:18 +0300

    [Create - ResultMessages] A Class contains constant messages or exceptions  of Result common messages

---
+++ Arccore.Result/Constants/ResultMessages.cs
@@ -0,0 +1,10 @@
+﻿namespace Arccore.Result.Constants;
+
+internal static class ResultMessages
+{
+    public const string SuccessResultException = "Success result has invalid result type ";
+    public const string FailureResultException = "Failure result has invalid result type ";
+    public const string NullError = "Can`t return failure result without error!";
+    public const string DefaultSuccess = "Success";
+    public const string DefaultError = "Error";
+}
